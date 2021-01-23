7a4d3b5d28f0422315bb70f8c986ed663dd23abc
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2021-01-22 18:37:14 -0600

    Update RadioGeneral.java

--- RadioGeneral.java
+++ RadioGeneral.java
@@ -1,1 +1,19 @@
 //RadioGeneral
+//--------------------------------------------------------------
+//Hoja de Trabajo #1
+//Creada por Esteban Aldana Guerra 20591 y Natanael Girón 20029
+//Algoritmos y Estructura de Datos
+//Ultima modificación: 18/01/2021
+//-----------------------------------------------------------------
+
+public interface RadioGeneral {
+
+    // Comprueba si la radio esta encendida, si no lo está no deja realizar otra
+    // acción hasta que se encienda la Radio
+    public boolean isON();
+
+    // Método encargado de encender la radio
+    public void encender();
+
+    // Método encargado de apagar la Radio
+    public void apagar();
