ba1eac92ae93651590cd9a05e764a9ca53e97510
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2021-01-22 18:36:27 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -45,3 +45,57 @@ public class Main {
                 scan.next();
             }
         }
+
+        while (radio.isON() == true) {
+            int op1 = 0;
+
+            try {
+
+                System.out.println(
+                        "A continuación encontrará diferentes opciones en las cuales podrá controlar de manera eficiente nuestra Radio Virtual\n");
+                System.out.println("1. Siguiente Emisora ");
+                System.out.println("2. Guardar Emisora ");
+                System.out.println("3. Elegir Modo (AM-FM)");
+                System.out.println("4. Seleccionar Emisora");
+                System.out.println("5. Apagar la Radio ");
+                op1 = scan.nextInt();
+
+                if (op1 == 1) {
+                    // Si el usuario elige la opcion 1
+                    radio.incrementar();
+
+                } else if (op1 == 2) {
+                    // Si el usuario elige la opcion 2
+                    System.out.println("¿En que espacio del 1 al 12 quisiera guardar la Emisora?\n");
+                    op3 = scan.nextInt();
+                    radio.asignar(op3);
+
+                } else if (op1 == 3) {
+                    // Si el usuario elige la opcion 3
+                    radio.frecuencia();
+
+                } else if (op1 == 4) {
+                    // Si elige opcion 5
+                    System.out.println("Seleccione la Emisora que quiere reproducir(1-12) ");
+                    op3 = scan.nextInt();
+                    radio.emisoras(op3);
+                    // mostrar lista de emisoras guardadas
+
+                } else if (op1 == 5) {
+                    // Si el usuario elige la opcion 5
+                    System.out.println("Apagando Radio...\n");
+                    radio.apagar();
+                    break;
+                } else {
+                    // Si el usuario no elige ninguna de las opciones anteriores
+                    System.out.println("Ingrese solamente una de las opciones mencionadas anteriormente\n");
+                }
+
+            } catch (Exception e) {
+
+                System.out.println("El número ingresado no es válido\n");
+                scan.next();
+            }
+        }
+    }
+}
